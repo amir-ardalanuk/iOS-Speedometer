0d020157ae32e56c0f811f7d08098e5548c26d59
amir.ardalanuk <amir.ardalanuk@gmail.com>
2020-03-22 20:53:30 +0430

    Initial Commit

---
+++ AALocationManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AALocationManager
+//
+//  Created by Amir Ardalan on 3/22/20.
+//  Copyright © 2020 Clean-Coder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AALocationManagerTests/AALocationManagerTests.swift
@@ -0,0 +1,34 @@
+//
+//  AALocationManagerTests.swift
+//  AALocationManagerTests
+//
+//  Created by Amir Ardalan on 3/22/20.
+//  Copyright © 2020 Clean-Coder. All rights reserved.
+//
+
+import XCTest
+@testable import AALocationManager
+
+class AALocationManagerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
